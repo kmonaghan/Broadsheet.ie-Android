08b4a2b0d1ff2261ba8d7857becf64ca23d7fa04
Karl Monaghan <karl.t.monaghan@gmail.com>
2013-05-17 22:09:53 +0100

    styling post page
    capturing tap to send your tube links to the youtube app

---
+++ assets/kmwordpress.js
@@ -0,0 +1,4 @@
+function changeFontSize(newSize)
+{
+    document.getElementById("singlentry").style.fontSize = newSize + "px";
+}
--- src/ie/broadsheet/app/fragments/PostDetailFragment.java
+++ src/ie/broadsheet/app/fragments/PostDetailFragment.java
@@ -5,6 +5,13 @@ import ie.broadsheet.app.PostDetailActivity;
 import ie.broadsheet.app.PostListActivity;
 import ie.broadsheet.app.R;
 import ie.broadsheet.app.model.json.Post;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -43,6 +50,7 @@ public class PostDetailFragment extends Fragment {
         }
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_post_detail, container, false);
@@ -62,10 +70,76 @@ public class PostDetailFragment extends Fragment {
             postHTML += "<div id='singlentry'>" + post.getContent() + "</div></div>";
             postHTML += "</div></body></html>";
 
-            webview.loadData(postHTML, "text/html", null);
-            webview.setWebViewClient(new WebViewClient());
+            webview.getSettings().setJavaScriptEnabled(true);
+            webview.loadDataWithBaseURL("file:///android_asset/", postHTML, "text/html", "UTF-8", null);
+            webview.setWebViewClient(new MyWebViewClient(this.getActivity()));
         }
 
         return rootView;
+    }
+
+    // Via http://stackoverflow.com/questions/14088623/android-webview-to-play-youtube-videos
+    public class MyWebViewClient extends WebViewClient {
+
+        public Activity mActivity;
+
+        public MyWebViewClient(Activity activity) {
+            super();
+            mActivity = activity;
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Uri uri = Uri.parse(url);
+            if (uri.getHost().contains("youtube.com")) {
+                viewYoutube(mActivity, url);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void viewYoutube(Context context, String url) {
+            viewWithPackageName(context, url, "com.google.android.youtube");
+        }
+
+        public void viewWithPackageName(Context context, String url, String packageName) {
+            try {
+                Intent viewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                if (isAppInstalled(context, packageName)) {
+                    viewIntent.setPackage(packageName);
+                }
+                context.startActivity(viewIntent);
+            } catch (Exception e) {
+                Intent viewIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                context.startActivity(viewIntent);
+            }
+        }
+
+        public boolean isAppInstalled(Context context, String packageName) {
+            PackageManager packageManager = context.getPackageManager();
+            try {
+                packageManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+                return true;
+            } catch (NameNotFoundException e) {
+            }
+            return false;
+        }
+
+        @Override
+        public void onPageFinished(final WebView view, String url) {
+            String javascript = "javascript:" + "var iframes = document.getElementsByTagName('iframe');"
+                    + "for (var i = 0, l = iframes.length; i < l; i++) {" + "   var iframe = iframes[i],"
+                    + "   a = document.createElement('a');" + "   a.setAttribute('href', iframe.src);"
+                    + "   d = document.createElement('div');" + "   d.style.width = iframe.offsetWidth + 'px';"
+                    + "   d.style.height = iframe.offsetHeight + 'px';" + "   d.style.top = iframe.offsetTop + 'px';"
+                    + "   d.style.left = iframe.offsetLeft + 'px';" + "   d.style.position = 'absolute';"
+                    + "   d.style.opacity = '0';" + "   d.style.filter = 'alpha(opacity=0)';"
+                    + "   d.style.background = 'black';" + "   a.appendChild(d);"
+                    + "   iframe.offsetParent.appendChild(a);" + "}";
+            view.loadUrl(javascript);
+
+            super.onPageFinished(view, url);
+        }
     }
 }
