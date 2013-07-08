f2a55b1bf5c4ec186150eab276de6c043155c24b
Karl Monaghan <karl.t.monaghan@gmail.com>
2013-07-08 22:35:59 +0100

    Code formatting

--- src/ie/broadsheet/app/adapters/CommentAdapter.java
+++ src/ie/broadsheet/app/adapters/CommentAdapter.java
@@ -19,108 +19,96 @@ import android.widget.TextView;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
-public class CommentAdapter extends ArrayAdapter<Comment> implements
-		View.OnClickListener {
-	public interface ReplyCommentListener {
-		public void onReply(int commentId);
-	}
+public class CommentAdapter extends ArrayAdapter<Comment> implements View.OnClickListener {
+    public interface ReplyCommentListener {
+        public void onReply(int commentId);
+    }
 
-	ReplyCommentListener mListener;
+    ReplyCommentListener mListener;
 
-	public static class ViewHolder {
-		public ImageView commentAvatar;
+    public static class ViewHolder {
+        public ImageView commentAvatar;
 
-		public TextView commentUser;
+        public TextView commentUser;
 
-		public TextView commentDate;
+        public TextView commentDate;
 
-		public TextView commentBody;
+        public TextView commentBody;
 
-		public Button replyButton;
-	}
+        public Button replyButton;
+    }
 
-	public void setReplyCommentListener(ReplyCommentListener mListener) {
-		this.mListener = mListener;
-	}
+    public void setReplyCommentListener(ReplyCommentListener mListener) {
+        this.mListener = mListener;
+    }
 
-	public CommentAdapter(Context context, int textViewResourceId,
-			List<Comment> comments) {
-		super(context, textViewResourceId, comments);
+    public CommentAdapter(Context context, int textViewResourceId, List<Comment> comments) {
+        super(context, textViewResourceId, comments);
 
-	}
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View v = convertView;
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
 
-		Activity activity = (Activity) getContext();
+        Activity activity = (Activity) getContext();
 
-		ViewHolder holder;
-		if (v == null) {
+        ViewHolder holder;
+        if (v == null) {
 
-			LayoutInflater vi = (LayoutInflater) activity
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			v = vi.inflate(R.layout.comment_list_item, parent, false);
-			holder = new ViewHolder();
-			holder.commentAvatar = (ImageView) v
-					.findViewById(R.id.commentAvatar);
-			holder.commentUser = (TextView) v.findViewById(R.id.commentUser);
-			holder.commentDate = (TextView) v.findViewById(R.id.commentDate);
-			holder.commentBody = (TextView) v.findViewById(R.id.commentBody);
-			holder.replyButton = (Button) v.findViewById(R.id.commentReply);
-			v.setTag(holder);
-		} else {
-			holder = (ViewHolder) v.getTag();
+            LayoutInflater vi = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(R.layout.comment_list_item, parent, false);
+            holder = new ViewHolder();
+            holder.commentAvatar = (ImageView) v.findViewById(R.id.commentAvatar);
+            holder.commentUser = (TextView) v.findViewById(R.id.commentUser);
+            holder.commentDate = (TextView) v.findViewById(R.id.commentDate);
+            holder.commentBody = (TextView) v.findViewById(R.id.commentBody);
+            holder.replyButton = (Button) v.findViewById(R.id.commentReply);
+            v.setTag(holder);
+        } else {
+            holder = (ViewHolder) v.getTag();
 
-			holder.commentAvatar.setImageDrawable(activity.getResources()
-					.getDrawable(R.drawable.default_user));
-		}
+            holder.commentAvatar.setImageDrawable(activity.getResources().getDrawable(R.drawable.default_user));
+        }
 
-		int standardMargin = activity.getResources().getDimensionPixelSize(
-				R.dimen.standard_margin);
+        int standardMargin = activity.getResources().getDimensionPixelSize(R.dimen.standard_margin);
 
-		final Comment comment = getItem(position);
+        final Comment comment = getItem(position);
 
-		int newMargin = standardMargin
-				+ (standardMargin * 2 * comment.getChildLevel());
-		v.setPadding(newMargin, v.getPaddingTop(), v.getPaddingRight(),
-				v.getPaddingBottom());
+        int newMargin = (standardMargin * 2 * comment.getChildLevel());
+        v.setPadding(newMargin, v.getPaddingTop(), v.getPaddingRight(), v.getPaddingBottom());
 
-		if (comment != null) {
-			String avatar = comment.getAvatar();
+        if (comment != null) {
+            String avatar = comment.getAvatar();
 
-			if ((avatar != null) && (avatar.length() > 0)) {
-				DisplayImageOptions options = new DisplayImageOptions.Builder()
-						.cacheInMemory().cacheOnDisc()
-						/*
-						 * .displayer( new
-						 * RoundedBitmapDisplayer(activity.getResources
-						 * ().getDimensionPixelSize(
-						 * R.dimen.standard_corner_radius)))
-						 */
-						.build();
+            if ((avatar != null) && (avatar.length() > 0)) {
+                DisplayImageOptions options = new DisplayImageOptions.Builder().cacheInMemory().cacheOnDisc()
+                /*
+                 * .displayer( new RoundedBitmapDisplayer(activity.getResources ().getDimensionPixelSize(
+                 * R.dimen.standard_corner_radius)))
+                 */
+                .build();
 
-				ImageLoader.getInstance().displayImage(avatar,
-						holder.commentAvatar, options);
-			}
+                ImageLoader.getInstance().displayImage(avatar, holder.commentAvatar, options);
+            }
 
-			holder.commentUser.setText(comment.getName());
-			holder.commentDate.setText(comment.getRelativeTime());
-			holder.commentBody.setText(Html.fromHtml(comment.getContent()));
+            holder.commentUser.setText(comment.getName());
+            holder.commentDate.setText(comment.getRelativeTime());
+            holder.commentBody.setText(Html.fromHtml(comment.getContent()));
 
-			holder.replyButton.setTag(position);
-			holder.replyButton.setOnClickListener(this);
-		}
+            holder.replyButton.setTag(position);
+            holder.replyButton.setOnClickListener(this);
+        }
 
-		return v;
-	}
+        return v;
+    }
 
-	@Override
-	public void onClick(View v) {
-		int position = (Integer) v.getTag();
+    @Override
+    public void onClick(View v) {
+        int position = (Integer) v.getTag();
 
-		final Comment comment = getItem(position);
+        final Comment comment = getItem(position);
 
-		mListener.onReply(comment.getId());
-	}
+        mListener.onReply(comment.getId());
+    }
 }
